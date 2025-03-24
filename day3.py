b29f8e9c672cf673396408aea19190779e94364e
Michael Carrillo <maichael10@gmail.com>
2025-03-24 14:53:51 -0700

    Use fileinput

--- day3.py
+++ day3.py
@@ -1,22 +1,24 @@
 import re
+import fileinput
+
+
 
 
 total_sum_multiplication = 0
 enabled = True
 
 
-with open('data/day3.input', 'r') as file:
-    # Read each line in the file
-    for line in file:
-        matches = re.findall(r'(mul\((\d+),(\d+)\))|(do\(\))|(don\'t\(\))', line)
-        for match in matches:
-            # print(match.string)
-            if match[-1] == "don't()":
-                enabled = False
-            elif match[-2] == "do()":
-                enabled = True
-            else:
-                if enabled:
-                    total_sum_multiplication += int(match[1]) * int(match[2])
+for line in fileinput.input():
+    matches = re.findall(r'(mul\((\d+),(\d+)\))|(do\(\))|(don\'t\(\))', line.rstrip())
+    for match in matches:
+        # print(match.string)
+        if match[-1] == "don't()":
+            enabled = False
+        elif match[-2] == "do()":
+            enabled = True
+        else:
+            if enabled:
+                total_sum_multiplication += int(match[1]) * int(match[2])
 
 print(total_sum_multiplication)
+# python day3.py < data/day3.input
